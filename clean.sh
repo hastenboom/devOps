20fd3cb52d10f07d6a7a49b6ec7191e06d73af43
hastenboom <782294940@qq.com>
2024-09-15 00:17:26 +0800

    bug fix

--- clean.sh
+++ clean.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 # Remove all containers and images with dev-ops prefix
 
-sudo docker container ls -a --format "{{.Names}}" | grep '^dev-ops' | xargs -I {} sudo docker -f rm {}
+# Remove containers
+echo "Removing containers..."
+sudo docker container ls -a --format "{{.Names}}" | grep '^dev-ops' | xargs -I {} sudo docker -f rm {} || echo "Error removing containers"
 
-sudo docker image ls | grep '^dev-ops' | awk '{print $3}' | xargs -I {} sudo docker rmi -f {}
+# Remove images
+echo "Removing images..."
+sudo docker image ls | grep '^dev-ops' | awk '{print $3}' | xargs -I {} sudo docker rmi -f {} || echo "Error removing images"
