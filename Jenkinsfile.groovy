29e7285da899ec15d97b65fc3e00802ccb05d0f2
hastenboom <782294940@qq.com>
2024-09-15 00:11:47 +0800

    bug fix

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -10,25 +10,25 @@ pipeline {
             steps {
                 /*   sh 'sudo docker container ls -a --format "{{.Names}}" | grep \'^dev-ops\' | xargs -I {} sudo docker -f rm {}'
                   sh ' sudo docker image ls | grep \'^dev-ops\' | awk \'{print $3}\' | xargs -I {} sudo docker rmi -f {}' */
-                /*  sh 'chmod +x clean.sh'
-                 sh 'chmod +x build.sh'
-                 // 执行当前目录下的clean.sh脚本
-                 sh 'sudo ./clean.sh'
-                 // 执行当前目录下的build.sh脚本
-                 sh 'sudo ./build.sh' */
                 script {
-                    echo "Listing all Docker containers..."
-                    sh "ls"
-                    sh 'sudo docker container ls -a --format "{{.Names}}"'
+                    /*  echo "Listing all Docker containers..."
+                      sh "ls"
+                      sh 'sudo docker container ls -a --format "{{.Names}}"'
 
-                    echo "Removing dev-ops containers..."
-                    sh 'sudo docker container ls -a --format "{{.Names}}" | grep "^dev-ops$" | xargs -rI {} sudo docker rm -f {}'
+                      echo "Removing dev-ops containers..."
+                      sh 'sudo docker container ls -a --format "{{.Names}}" | grep "^dev-ops$" | xargs -rI {} sudo docker rm -f {}'
 
-                    echo "Listing all Docker images..."
-                    sh 'sudo docker image ls'
+                      echo "Listing all Docker images..."
+                      sh 'sudo docker image ls'
 
-                    echo "Removing dev-ops images..."
-                    sh 'sudo docker image ls | grep "^dev-ops$" | awk "{print \$3}" | xargs -rI {} sudo docker rmi -f {}'
+                      echo "Removing dev-ops images..."
+                      sh 'sudo docker image ls | grep "^dev-ops$" | awk "{print \$3}" | xargs -rI {} sudo docker rmi -f {}'*/
+                    sh 'chmod +x clean.sh'
+                    sh 'chmod +x build.sh'
+                    // 执行当前目录下的clean.sh脚本
+                    sh 'sudo ./clean.sh'
+                    // 执行当前目录下的build.sh脚本
+                    sh 'sudo ./build.sh'
                 }
             }
 
@@ -37,7 +37,7 @@ pipeline {
             steps {
                 script {
                     sh 'mvn clean package'
-                    sh 'sudo docker compose up --build'
+                    sh 'sudo docker compose up --build -d'
                 }
             }
         }
